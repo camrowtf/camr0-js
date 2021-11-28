a99a57fe0b6df0ce4f4326b7bf043e5bd9041a43
camrowtf <ccamro@outlook.com>
2021-11-28 19:26:52 +0000

    Added cool server info command lelelelel

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Require the necessary discord.js classes
-const {Client, Intents, IntegrationApplication } = require('discord.js');
+const { channelMention } = require('@discordjs/builders');
+const {Client, Intents, IntegrationApplication, Message } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const { token } = require('./config.json');
 
 // Creating a new client instance
@@ -20,7 +22,13 @@ client.on('interactionCreate', async interaction => {
     if (commandName === 'ping') {
         await interaction.reply('Pong : )');
     } else if (commandName === 'server') {
-        await interaction.reply(`Server name > ${interaction.guild.name}\nTotal members > ${interaction.guild.memberCount}\nDate created > ${interaction.guild.createdTimestamp}\n NSFW status > ${interaction.guild.nsfwLevel}`);
+        const serverEmbed = new MessageEmbed()
+        .setColor('BLURPLE')
+        .setTitle('Server info')
+        .setImage('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.springfrog.com%2Fconverter%2Fascii-text-art%2Fgallery%2Frose.gif&f=1&nofb=1')
+        .setURL('https://discord.gg/XFRRM4uvGx')
+        .setDescription(`**Server name:** ${interaction.guild.name}\n **Server ID:** ${interaction.guild.id}\n  **Total members:** ${interaction.guild.memberCount}\n **Date created:** ${interaction.guild.createdAt}\n **Joined at:** ${interaction.guild.joinedAt}`)
+        await interaction.reply({embeds: [serverEmbed]});
     } else if (commandName === 'user') {
         await interaction.reply('User info.');
     }
