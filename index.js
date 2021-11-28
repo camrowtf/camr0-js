55cd1bd9859d4e6a61dee2383775a22f845e2cbc
camrowtf <ccamro@outlook.com>
2021-11-28 19:30:25 +0000

    Added server info command

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ client.on('interactionCreate', async interaction => {
         await interaction.reply('Pong : )');
     } else if (commandName === 'server') {
         const serverEmbed = new MessageEmbed()
-        .setColor('BLURPLE')
-        .setTitle('Server info')
-        .setImage('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.springfrog.com%2Fconverter%2Fascii-text-art%2Fgallery%2Frose.gif&f=1&nofb=1')
-        .setURL('https://discord.gg/XFRRM4uvGx')
-        .setDescription(`**Server name:** ${interaction.guild.name}\n **Server ID:** ${interaction.guild.id}\n  **Total members:** ${interaction.guild.memberCount}\n **Date created:** ${interaction.guild.createdAt}\n **Joined at:** ${interaction.guild.joinedAt}`)
+            .setColor('BLURPLE')
+            .setTitle('Server info')
+            .setImage('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.springfrog.com%2Fconverter%2Fascii-text-art%2Fgallery%2Frose.gif&f=1&nofb=1')
+            .setURL('https://discord.gg/XFRRM4uvGx')
+            .setDescription(`**Server name:** ${interaction.guild.name}\n **Server ID:** ${interaction.guild.id}\n  **Total members:** ${interaction.guild.memberCount}\n **Date created:** ${interaction.guild.createdAt}\n **Joined at:** ${interaction.guild.joinedAt}`)
         await interaction.reply({embeds: [serverEmbed]});
     } else if (commandName === 'user') {
         await interaction.reply('User info.');
